e769578c4d650fb37e81d660ab4429c8b9ec5019
okuninoshi <gennesis@outlook.fr>
2020-01-16 21:08:07 +0500

    commit

--- jobHunt/Scripts.js
+++ jobHunt/Scripts.js
@@ -2,11 +2,21 @@ $(window).scroll(function(){
 	$('nav').toggleClass('scrolled', $(this).scrollTop() > 50);
 });
 // Get the modal
-var modal = document.getElementById('id01');
+// var modal = document.getElementById('signupForm');
 
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function (event) {
-	if (event.target == modal) {
-		modal.style.display = 'none';
-	}
-};
+// // When the user clicks anywhere outside of the modal, close it
+// window.onclick = function (event) {
+// 	if (event.target == modal) {
+// 		modal.style.display = 'none';
+// 	}
+// };
+
+
+// var loginModal = document.getElementById('loginForm');
+
+// // When the user clicks anywhere outside of the modal, close it
+// window.onclick = function (event) {
+// 	if (event.target == loginModal) {
+// 		loginModal.style.display = 'none';
+// 	}
+// };
